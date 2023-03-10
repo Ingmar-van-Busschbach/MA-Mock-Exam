8d5a777b6dcaf6fe8d8d739ae20e7f4e25acbe83
Miguel-Heijmans <58071327+Miguel-Heijmans@users.noreply.github.com>
2023-03-10 18:13:42 +0100

    reverted my script to an older version
    i had to revert my script to an older version because things just went that badly with it during the merge. this version i have confirmed works and is stable and will be used as a "control" so i can test out what the problem was step by step.

--- Project Files/Unity/Assets/scripts/Spawn.cs
+++ Project Files/Unity/Assets/scripts/Spawn.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
@@ -8,13 +7,13 @@ using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch;
 [RequireComponent (typeof(ARRaycastManager), typeof(ARPlaneManager))]
 public class Spawn : MonoBehaviour
 {
-    [SerializeField] private GameObject[] prefabs;
+
+    [SerializeField]
+    private GameObject prefab;
 
     private ARRaycastManager aRRaycastManager;
     private ARPlaneManager aRPlaneManager;
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
-
-    private List<GameObject> instantiatedObjects = new List<GameObject>();
 
     // Start is called before the first frame update
     private void Awake()
@@ -23,11 +22,11 @@ public class Spawn : MonoBehaviour
         aRPlaneManager = GetComponent<ARPlaneManager>();
     }
 
-    
+
    /// <summary>
    /// This function is called when the object becomes enabled and active.
    /// </summary>
-   /*private void OnEnable()
+   private void OnEnable()
    {
         EnhancedTouch.TouchSimulation.Enable();
         EnhancedTouch.EnhancedTouchSupport.Enable();
@@ -43,75 +42,33 @@ public class Spawn : MonoBehaviour
         EnhancedTouch.EnhancedTouchSupport.Disable();
         EnhancedTouch.Touch.onFingerDown -= FingerDown;
    }
-   */
 
-    public void SpawnPrefab()
-    {
-         //if (finger.index != 0) return;
-    
-         //get coordinates of where the user touched on the screen
-         if (!aRRaycastManager.Raycast(Camera.main.transform.forward, hits, TrackableType.PlaneWithinPolygon))
-         {
-             return;
-         }
-         foreach (ARRaycastHit hit in hits)
-         {
-              Pose pose = hit.pose;
-              GameObject obj = Instantiate(prefabs[0], pose.position, pose.rotation);
-             instantiatedObjects.Add(obj);
-              
-              if (aRPlaneManager.GetPlane(hit.trackableId).alignment == PlaneAlignment.HorizontalUp)
-              {
-                   Vector3 position = obj.transform.position;
-                   
-                   Vector3 cameraPosition = Camera.main.transform.position;
-                   
-                   Vector3 direction = cameraPosition - position;
-                   Vector3 targetRotationEuler = Quaternion.LookRotation(direction).eulerAngles;
-                   Vector3 scaledEuler = Vector3.Scale(targetRotationEuler, obj.transform.up.normalized);
-                   Quaternion targetRotation = Quaternion.Euler(scaledEuler);
-                   obj.transform.rotation = obj.transform.rotation * targetRotation;
-              }
-         }
-    }
-
-    public void SpawnPrefabImportant()
-    {
+   private void FingerDown(EnhancedTouch.Finger finger)
+   {
         //if (finger.index != 0) return;
 
         //get coordinates of where the user touched on the screen
-        if (!aRRaycastManager.Raycast(Camera.main.transform.forward, hits, TrackableType.PlaneWithinPolygon))
+        if (aRRaycastManager.Raycast(finger.currentTouch.screenPosition, hits, TrackableType.PlaneWithinPolygon))
         {
-            return;
-        }
-        foreach (ARRaycastHit hit in hits)
-        {
-            Pose pose = hit.pose;
-            GameObject obj = Instantiate(prefabs[1], pose.position, pose.rotation);
-            instantiatedObjects.Add(obj);
-
-            if (aRPlaneManager.GetPlane(hit.trackableId).alignment == PlaneAlignment.HorizontalUp)
+            foreach (ARRaycastHit hit in hits)
             {
-                Vector3 position = obj.transform.position;
+               Pose pose = hit.pose;
+               GameObject obj = Instantiate(prefab, pose.position, pose.rotation);
 
-                Vector3 cameraPosition = Camera.main.transform.position;
+               if (aRPlaneManager.GetPlane(hit.trackableId).alignment == PlaneAlignment.HorizontalUp)
+               {
+                    Vector3 position = obj.transform.position;
 
-                Vector3 direction = cameraPosition - position;
-                Vector3 targetRotationEuler = Quaternion.LookRotation(direction).eulerAngles;
-                Vector3 scaledEuler = Vector3.Scale(targetRotationEuler, obj.transform.up.normalized);
-                Quaternion targetRotation = Quaternion.Euler(scaledEuler);
-                obj.transform.rotation = obj.transform.rotation * targetRotation;
+                    Vector3 cameraPosition = Camera.main.transform.position;
+
+                    Vector3 direction = cameraPosition - position;
+                    Vector3 targetRotationEuler = Quaternion.LookRotation(direction).eulerAngles;
+                    Vector3 scaledEuler = Vector3.Scale(targetRotationEuler, obj.transform.up.normalized);
+                    Quaternion targetRotation = Quaternion.Euler(scaledEuler);
+                    obj.transform.rotation = obj.transform.rotation * targetRotation;
+               }
             }
         }
-    }
-
-    public void DestroyPrefab()
-   {
-     
-          foreach (GameObject instantiatedObject in instantiatedObjects)
-          {
-               Destroy(instantiatedObject);
-          }
    }
 
 
